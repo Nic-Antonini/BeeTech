edfa4f34b3de407b26fa15a9918b4b7e32aa2ca0
Nic-Antonini <121265107+Nic-Antonini@users.noreply.github.com>
2024-02-29 00:39:35 -0300

    29/02
    as 00:40

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,34 +1,65 @@
-import React from "react";
+"use client";
+import React,{useLayoutEffect} from "react";
 import Style from "./page.module.css";
-import Logo from '../assets/img/logo.png'
+import Logo from '../assets/img/logo.png';
+import FarmerBeekeeper from '../assets/img/api-agri.svg';
+import {gsap} from 'gsap';
+import {ScrollTrigger} from 'gsap-trial/ScrollTrigger';
 
-export default () =>{
+export default () => {
+
+  useLayoutEffect(()=>{
+    gsap.to(`.${Style.ilustration}`, {
+      duration: 1, x: 0, opacity: 1
+    })
+  }, [])
+
   return(
+
     <body className={Style.body}>
       
       <header className={Style.header}>
 
-        <div className={Style.logoArea}>
+      <div className={Style.menu}>
 
-        <img className={Style.logo} src={Logo.src} alt="" />
+          <div className={Style.logoArea}>
+
+          <img className={Style.logo} src={Logo.src} alt="Logo do sistema BeeTech" />
+
+          </div>
+
+          <nav className={Style.navbar}>
+
+            <a href="#" className={Style.navObj}>Sobre nós</a>
+
+            <a href="#" className={Style.navObj}>FAQ</a>
+
+            <a href="#" className={Style.navBtn}>
+
+              <button className={Style.btnLogin}>
+                Entrar
+              </button>
+
+            </a>
+
+          </nav>
 
         </div>
 
-        <nav className={Style.navbar}>
+        <div className={Style.contentMenu}>
+          
+          <h1 className={Style.pageDesc}>
+          Unindo <span className={Style.span}> Criadores de Abelhas </span>
+          e <span className={Style.span}> Produtores Rurais </span>
+         Rumo a um Futuro <span className={Style.span}> Sustentável </span>
+          e <span className={Style.span}>Tecnológico</span>.
+          </h1>
 
-          <a href="#" className={Style.navObj}>Sobre nós</a>
+          <img className={Style.ilustration} src={FarmerBeekeeper.src}
+          alt="Uma ilustração de um homem agricultor com uma caixa de maçãs
+          e uma mulher apicultora segurando um favo de abelha." />
 
-          <a href="#" className={Style.navObj}>FAQ</a>
-
-          <a href="#" className={Style.navObj}>
-
-            <button className={Style.btnLogin}>
-              Entrar
-            </button>
-
-          </a>
-
-        </nav>
+        </div>
 
       </header>
 
@@ -37,5 +68,6 @@ export default () =>{
       </main>
 
     </body>
+
   )
 }
